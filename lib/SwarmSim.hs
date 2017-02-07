4666ef7e2dc3d869a2860b9dcf9800225366ec83
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2017-02-07 07:03:51 -0500

    chg:dev: rename qualified import of hmatrix

--- lib/SwarmSim.hs
+++ lib/SwarmSim.hs
@@ -11,7 +11,7 @@ import Graphics.Gloss.Data.ViewPort (ViewPort)
 
 import qualified System.IO.Streams as Stream
 
-import qualified Numeric.LinearAlgebra as V
+import qualified Numeric.LinearAlgebra as LA
 
 -- http://www.snoyman.com/blog/2016/11/haskells-missing-concurrency-basics
 -- say = S8.putStrLn . encodeUtf8
@@ -23,26 +23,26 @@ box = Line [(0,0), (0,100), (100,100), (100,0), (0,0)]
 box_size :: Float
 box_size = realToFrac norm
   where
-    norm = V.norm_2 v
-    v = V.fromList [100, 100] :: V.Vector Float
+    norm = LA.norm_2 v
+    v = LA.fromList [100, 100] :: LA.Vector Float
 
 data Agent = Agent
-             { pos :: V.Vector Float
-             , vel :: V.Vector Float
-             , acc :: V.Vector Float
+             { pos :: LA.Vector Float
+             , vel :: LA.Vector Float
+             , acc :: LA.Vector Float
              } deriving Show
 
 type Model = Agent
 
 initialModel :: Model
-initialModel = Agent (V.fromList [0, 0]) (V.fromList [10, 10]) (V.fromList [100, -10])
+initialModel = Agent (LA.fromList [0, 0]) (LA.fromList [10, 10]) (LA.fromList [100, -10])
 
 modelToPic :: Model -> Picture
 modelToPic a = p
   where
     r = pos a
-    [rx, ry] = V.toList $ r
-    [vx, vy] = V.toList $ r + vel a
+    [rx, ry] = LA.toList $ r
+    [vx, vy] = LA.toList $ r + vel a
     position = Translate rx ry $ Circle 5
     velocity = Line [(rx, ry), (vx, vy)]
     p = Pictures [box, traceShow velocity velocity, position]
@@ -53,9 +53,9 @@ nextModel delta agent = agent'
   where
     agent' = Agent r v a
     -- r = r0 + vt + (1/2)at^2
-    r = (pos agent) + (V.scale delta (vel agent)) + (V.scale (0.5*delta**2) (acc agent) )
+    r = (pos agent) + (LA.scale delta (vel agent)) + (LA.scale (0.5*delta**2) (acc agent) )
     -- v = at + v0
-    v = (vel agent) + (V.scale delta (acc agent))
+    v = (vel agent) + (LA.scale delta (acc agent))
     a = acc agent
 
 
@@ -67,8 +67,8 @@ makePeriodic :: Agent -> Agent
 makePeriodic a = a { pos = r' }
   where
     r = pos a
-    r' :: V.Vector Float
-    r' = V.cmap (\r -> r - fromIntegral(floor(r / 100)) * 100) r
+    r' :: LA.Vector Float
+    r' = LA.cmap (\r -> r - fromIntegral(floor(r / 100)) * 100) r
 
 
 
