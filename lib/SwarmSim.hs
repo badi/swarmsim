cc454a4a844023d2a47307c2c49dd9b6a0f46c04
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2017-02-09 23:19:48 -0500

    chg:dev: save

--- lib/SwarmSim.hs
+++ lib/SwarmSim.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
@@ -7,13 +8,36 @@ module SwarmSim where
 
 import Debug.Trace
 
-import Graphics.Gloss
-import Graphics.Gloss.Data.ViewPort (ViewPort)
+-- import Graphics.Gloss
+-- import Graphics.Gloss.Data.ViewPort (ViewPort)
 
 import qualified System.IO.Streams as Stream
 
--- import qualified Data.Vector
-import qualified Numeric.LinearAlgebra as LA
+-- import Data.Vector.Instances
+
+import Linear as L
+import qualified Linear.V as L
+import qualified Data.Vector as V
+import Control.Lens (FoldableWithIndex(..))
+
+type Vector a = V.Vector a
+type Matrix a = V.Vector (Vector a)
+
+
+v,w,x :: Vector Double
+v = V.fromList [2, 2]
+w = 5*^v
+x = V.fromList [1, 2]
+
+m,n :: Matrix Double
+m = V.fromList [v, x]
+n = 2*!!m
+
+-- -- | Compute the distance matrix between the vector and every row in the matrix.
+-- distanceMatrix ::  Floating a => Vector a -> Matrix a -> V.Vector a
+-- distanceMatrix v m = V.map (distance v) m
+
+
 
 -- http://www.snoyman.com/blog/2016/11/haskells-missing-concurrency-basics
 -- say = S8.putStrLn . encodeUtf8
@@ -48,129 +72,209 @@ data F
 
 
 type Mass = Tagged M Double
-type Masses = Tagged M (LA.Vector Double)
-
-type Position = Tagged X (LA.Vector Double)
-type Positions = Tagged X (LA.Matrix Double)
-
-type Velocity = Tagged V (LA.Vector Double)
-type Velocities = Tagged V (LA.Matrix Double)
-
-type Acceleration = Tagged A (LA.Vector Double)
-type Accelerations = Tagged A (LA.Matrix Double)
-
-type Force = Tagged F (LA.Vector Double)
-type Forces = Tagged F (LA.Matrix Double)
-
+type Position = Tagged X (Vector Double)
+type Velocity = Tagged V (Vector Double)
+type Acceleration = Tagged A (Vector Double)
+type Force = Tagged F (Vector Double)
 type TimeStep = Double
-
+type Charge = Double
+type Index = Int
+type Distance = Double
+type Neighborhood = Vector Index
+type Neighbor = Position
 
 data State =
   MkState
-  { stMass  :: !Masses
-  , stPos   :: !Positions
-  , stVel   :: !Velocities
-  , stAcc   :: !Accelerations
+  { stMass  :: !(V.Vector Mass)
+  , stCharge:: !(V.Vector Charge)
+  , stPos   :: !(V.Vector Position)
+  , stVel   :: !(V.Vector Velocity)
+  , stAcc   :: !(V.Vector Acceleration)
   } deriving Show
 
 data PotentialParams =
   MkPotentialParams
-  { ppCuttoff :: !Double
-  , ppRadius :: !Double
-  , ppCharge :: !Double
-  , ppOrder :: !Int
+  { ppOrder :: !Int
+  , ppConstant :: !Double
   } deriving Show
 
-potential :: PotentialParams -> Force
-potential pp = fs
+
+distances :: Floating a => Matrix a -> Matrix a
+distances m = fmap (flip distancesTo m) m
+
+
+distancesTo :: Floating a => Vector a -> Matrix a -> Vector a
+distancesTo v m = fmap (distance v) m
+
+
+whence :: (FoldableWithIndex i f, Applicative f, Monoid (f i)) => (a -> Bool) -> f a -> f i
+whence p xs = ifoldMap (\i x -> if p x then pure i else mempty) xs
+
+
+neighbors :: Distance -> Position -> Vector Position -> Neighborhood
+neighbors c vT mT = ns
   where
-    fs = undefined
+    v = unTag vT
+    m = V.map unTag mT
+    ns = whence (== True) $ fmap (<= c) $ distancesTo v m
 
-velocityVerlet :: TimeStep -> State -> State
-velocityVerlet dt s = s'
+
+-- | Find the nearest neighbors
+--
+-- Given an index `i`, the set of all points `ps` and their cutoff
+-- radii `xs`, return the set of neighbors within `xs[i]` that are not
+-- `i`.
+findNeighbors :: Vector Position -> Vector Distance -> Index -> Neighborhood
+findNeighbors ps xs i = ns
   where
-    x = unTag $! stPos s
-    v = unTag $! stVel s
-    a = unTag $! stAcc s
-    m = unTag $! stMass s
+    p = ps V.! i
+    x = xs V.! i
 
-    -- x⃗(t+Δt) = x⃗(t) + v⃗(t)Δt + 0.5a⃗(t)Δt²
-    x' = x + dt`LA.scale`v + (0.5*dt^^2)`LA.scale`a
+    without :: Index -> Neighborhood -> Neighborhood
+    without i' ps' = V.filter (/= i') ps'
 
-    f  = undefined
-    a' = undefined
+    ns :: Neighborhood
+    ns = without i $ neighbors x p ps
 
-    -- v⃗(t+Δt)=v⃗(t)+0.5t(a⃗(t)+a⃗(t+Δt))Δt
-    v' = v + (0.5*dt)`LA.scale`(a+a')
 
-    s' = s
-         { stPos = Tagged x'
-         , stVel = Tagged v'
-         , stAcc = Tagged a'
-         }
+neighborhood :: Vector a -> Neighborhood -> Vector a
+neighborhood v ixs = V.map (\i -> v V.! i) ixs
+    
+
+pp = MkPotentialParams 3 1
+a :: Position
+a = Tagged $ V.fromList [1,2]
+q = 1.0
+bs :: Vector Position
+bs = V.fromList $ map Tagged [v,w]
+qs :: Vector Double
+qs = V.map (const 1) bs
+rs :: Vector Distance
+rs = V.replicate (V.length qs) 99
+
+t1 = coulombForces pp a q bs qs
+t2 = findNeighbors bs rs 0
+t3 = forces pp bs rs qs
+
+coulombForces :: PotentialParams -> Position -> Double -> Vector Position -> Vector Double -> Force
+coulombForces pp x1T q1 ysT qs = Tagged f
+  where
+    x1 = unTag x1T
+    ys = V.map unTag ysT
+    effects y2 q2 = q2 *^ normalize (x1^-^y2) ^/ norm (x1^-^y2) ^ (ppOrder pp)
+    summation = V.foldl1 (^+^) $ V.zipWith effects ys qs
+    f = q1 / (ppConstant pp) *^ summation
+
+
+forces :: PotentialParams -> Vector Position -> Vector Distance -> Vector Charge -> Vector Force
+forces pp ps xs qs = V.map computeForce indices
+  where
+    -- [0,1,...,n]
+    indices = V.generate (V.length ps) (+1)
+
+    -- indices of the neighbors for each point
+    ns :: Vector Neighborhood
+    ns = V.map (findNeighbors ps xs) indices
+
+    -- subset of coordinates, cutoffs, and chargs
+    subset superset i = neighborhood superset i
+    ps' :: Vector (Vector Position)
+    xs' :: Vector (Vector Distance)
+    qs' :: Vector (Vector Charge)
+    ps' = V.map (subset ps) ns
+    xs' = V.map (subset xs) ns
+    qs' = V.map (subset qs) ns
+
+    computeForce :: Index -> Force
+    computeForce i = coulombForces pp (ps V.! i) (xs V.! i) (ps' V.! i) (qs' V.! i)
+
+
+
+-- velocityVerlet :: TimeStep -> State -> State
+-- velocityVerlet dt s = s'
+--   where
+--     x = unTag $! stPos s
+--     v = unTag $! stVel s
+--     a = unTag $! stAcc s
+--     m = unTag $! stMass s
+
+--     -- x⃗(t+Δt) = x⃗(t) + v⃗(t)Δt + 0.5a⃗(t)Δt²
+--     x' = x + dt*v + (0.5*dt^^2)*a
+
+--     f  = undefined
+--     a' = undefined
+
+--     -- v⃗(t+Δt)=v⃗(t)+0.5t(a⃗(t)+a⃗(t+Δt))Δt
+--     v' = v + (0.5*dt) * (a+a')
+
+--     s' = s
+--          { stPos = Tagged x'
+--          , stVel = Tagged v'
+--          , stAcc = Tagged a'
+--          }
   
 
 
 
-box = Line [(0,0), (0,100), (100,100), (100,0), (0,0)]
+-- box = Line [(0,0), (0,100), (100,100), (100,0), (0,0)]
 
-box_size :: Float
-box_size = realToFrac norm
-  where
-    norm = LA.norm_2 v
-    v = LA.fromList [100, 100] :: LA.Vector Float
+-- box_size :: Float
+-- box_size = realToFrac norm
+--   where
+--     norm = LA.norm_2 v
+--     v = LA.fromList [100, 100] :: Vector Float
 
-data Agent = Agent
-             { pos :: LA.Vector Float
-             , vel :: LA.Vector Float
-             , acc :: LA.Vector Float
-             } deriving Show
+-- data Agent = Agent
+--              { pos :: Vector Float
+--              , vel :: LA.Vector Float
+--              , acc :: LA.Vector Float
+--              } deriving Show
 
-type Model = Agent
+-- type Model = Agent
 
-initialModel :: Model
-initialModel = Agent (LA.fromList [0, 0]) (LA.fromList [10, 10]) (LA.fromList [100, -10])
+-- initialModel :: Model
+-- initialModel = Agent (LA.fromList [0, 0]) (LA.fromList [10, 10]) (LA.fromList [100, -10])
 
-modelToPic :: Model -> Picture
-modelToPic a = p
-  where
-    r = pos a
-    [rx, ry] = LA.toList $ r
-    [vx, vy] = LA.toList $ r + vel a
-    position = Translate rx ry $ Circle 5
-    velocity = Line [(rx, ry), (vx, vy)]
-    p = Pictures [box, traceShow velocity velocity, position]
-
-
-nextModel :: Float -> Model -> Model
-nextModel delta agent = agent'
-  where
-    agent' = Agent r v a
-    -- r = r0 + vt + (1/2)at^2
-    r = (pos agent) + (LA.scale delta (vel agent)) + (LA.scale (0.5*delta**2) (acc agent) )
-    -- v = at + v0
-    v = (vel agent) + (LA.scale delta (acc agent))
-    a = acc agent
+-- modelToPic :: Model -> Picture
+-- modelToPic a = p
+--   where
+--     r = pos a
+--     [rx, ry] = LA.toList $ r
+--     [vx, vy] = LA.toList $ r + vel a
+--     position = Translate rx ry $ Circle 5
+--     velocity = Line [(rx, ry), (vx, vy)]
+--     p = Pictures [box, traceShow velocity velocity, position]
 
 
-step :: ViewPort -> Float -> Model -> Model
-step _ t = makePeriodic . nextModel t
+-- nextModel :: Float -> Model -> Model
+-- nextModel delta agent = agent'
+--   where
+--     agent' = Agent r v a
+--     -- r = r0 + vt + (1/2)at^2
+--     r = (pos agent) + (LA.scale delta (vel agent)) + (LA.scale (0.5*delta**2) (acc agent) )
+--     -- v = at + v0
+--     v = (vel agent) + (LA.scale delta (acc agent))
+--     a = acc agent
 
 
-makePeriodic :: Agent -> Agent
-makePeriodic a = a { pos = r' }
-  where
-    r = pos a
-    r' :: LA.Vector Float
-    r' = LA.cmap (\r -> r - fromIntegral(floor(r / 100)) * 100) r
+-- step :: ViewPort -> Float -> Model -> Model
+-- step _ t = makePeriodic . nextModel t
+
+
+-- makePeriodic :: Agent -> Agent
+-- makePeriodic a = a { pos = r' }
+--   where
+--     r = pos a
+--     r' :: LA.Vector Float
+--     r' = LA.cmap (\r -> r - fromIntegral(floor(r / 100)) * 100) r
 
 
 
-test =
-  simulate
-    (InWindow "Agents" (100, 100) (0, 0))
-    white
-    120
-    initialModel
-    modelToPic
-    step
+-- test =
+--   simulate
+--     (InWindow "Agents" (100, 100) (0, 0))
+--     white
+--     120
+--     initialModel
+--     modelToPic
+--     step
